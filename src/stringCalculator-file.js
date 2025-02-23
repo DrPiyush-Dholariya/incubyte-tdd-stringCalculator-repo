e9f6b62dea55cf6ddb6a4a5ee74d01a7da4b0609
Dr. Piyush Dholariya <piyushkumar@backbase.com>
2025-02-23 13:42:35 +0530

    Add logic to handle custom delimiter

--- src/stringCalculator-file.js
+++ src/stringCalculator-file.js
@@ -3,7 +3,15 @@ function add(numbers) {
     return 0;
   }
 
-  const numbersArr = numbers.replace('\n', ',').split(',');
+  let delimiter = /[\n,]/;
+  if (numbers.startsWith('//')) {
+    const parts = numbers.split('\n');
+    delimiter = new RegExp(`[${parts[0].slice(2)}]`);
+    numbers = parts[1];
+  }
+
+  const numbersArr = numbers.split(delimiter);
+
   const sum = numbersArr.reduce((total, num) => total + parseInt(num, 10), 0);
 
   return sum;
--- test/stringCalculator-file.test.js
+++ test/stringCalculator-file.test.js
@@ -20,5 +20,10 @@ test('For any number of comma separated numbers should return their sum', () => 
 });
 
 test('new line instead of comma between numbers', () => {
-    expect(add('1\n2,3')).toBe(6);
+  expect(add('1\n2,3')).toBe(6);
+});
+
+test('support custom delimiter with a pattern', () => {
+  expect(add('//;\n1;2')).toBe(3);
+  expect(add('//?\n1?2?3')).toBe(6);
 });
