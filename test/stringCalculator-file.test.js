943c6afd55e42c9cb761e6dbe1245c8f4cc3b366
Piyushkumar Dholariya <piyushkumar@backbase.com>
2025-02-23 13:21:49 +0530

    Failing test case for single number for add func

--- test/stringCalculator-file.test.js
+++ test/stringCalculator-file.test.js
@@ -1,5 +1,9 @@
 const add = require('../src/stringCalculator-file');
 
 test('returns 0 for an empty string', () => {
-  expect(add("")).toBe(0);
+  expect(add('')).toBe(0);
+});
+
+test('returns number for a single element in the string', () => {
+  expect(add('1')).toBe(1);
 });
